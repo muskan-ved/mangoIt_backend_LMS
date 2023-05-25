f232a8dfc0dd3e3cf9ac2165db60d46ed4a1d0ee
varun <varun.mangoit@gmail.com>
2023-05-25 19:35:37 +0530

    worked on subscription controller

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,81 +1,105 @@
-const db = require('../models/index.model')
-require('dotenv').config()
-const jsonwebtoken = require('jsonwebtoken')
-const Subscription = db.Subscription
+const db = require("../models/index.model");
+require("dotenv").config();
+const jsonwebtoken = require("jsonwebtoken");
+const Subscription = db.Subscription;
 
 exports.createSubcsription = async (req, res) => {
-    const {
-        name,
-        description,
-        price,
-        duration_term,
-        duration_value,
-    } = req.body
+  const { name, description, price, duration_term, duration_value } = req.body;
 
-    const token = req.headers.logintoken
-    const decode = jsonwebtoken.verify(token, process.env.SIGNING_KEY)
-    const created_by = decode.id
+  const token = req.headers.logintoken;
+  const decode = jsonwebtoken.verify(token, process.env.SIGNING_KEY);
+  const created_by = decode.id;
 
-
-    try {
-        const createSubscription = await Subscription.create({
-            name: name,
-            description: description,
-            price: price,
-            duration_term: duration_term,
-            duration_value: duration_value,
-            created_by: created_by,
-        })
-        res.status(201).json(createSubscription)
-
-    }
-    catch (e) {
-        res.status(400).json(e)
-    }
-}
+  try {
+    const createSubscription = await Subscription.create({
+      name: name,
+      description: description,
+      price: price,
+      duration_term: duration_term,
+      duration_value: duration_value,
+      created_by: created_by,
+    });
+    res.status(201).json(createSubscription);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
 
 exports.updateSubscription = async (req, res) => {
-    const {
-        name,
-        description,
-        price,
-        duration_term,
-        duration_value,
-    } = req.body
+  const { name, description, price, duration_term, duration_value } = req.body;
 
-    const subscriptionId = req.params.id
+  const subscriptionId = req.params.id;
 
-    const token = req.headers.logintoken
-    const decode = jsonwebtoken.verify(token, process.env.SIGNING_KEY)
-    const updated_by = decode.id
+  const token = req.headers.logintoken;
+  const decode = jsonwebtoken.verify(token, process.env.SIGNING_KEY);
+  const updated_by = decode.id;
 
-    const updateSubscription = await Subscription.update({
-        name: name,
-        description: description,
-        price: price,
-        duration_term: duration_term,
-        duration_value: duration_value,
-        updated_by: updated_by,
-    }, { where: { id: subscriptionId } })
+  const updateSubscription = await Subscription.update(
+    {
+      name: name,
+      description: description,
+      price: price,
+      duration_term: duration_term,
+      duration_value: duration_value,
+      updated_by: updated_by,
+    },
+    { where: { id: subscriptionId } }
+  );
 
-    const updatedSubscriptionValue = await Subscription.findOne({ where: { id: subscriptionId } })
-    res.send(updatedSubscriptionValue)
-}
+  const updatedSubscriptionValue = await Subscription.findOne({
+    where: { id: subscriptionId },
+  });
+  res.send(updatedSubscriptionValue);
+};
 
 exports.deleteSubscription = async (req, res) => {
-    const subscriptionId = req.params.id
+  const subscriptionId = req.params.id;
 
-    const token = req.headers.logintoken
-    const decode = jsonwebtoken.verify(token, process.env.SIGNING_KEY)
-    const deletedBy = decode.id
+  const token = req.headers.logintoken;
+  const decode = jsonwebtoken.verify(token, process.env.SIGNING_KEY);
+  const deletedBy = decode.id;
 
-    try {
-        const isDeleted = await Subscription.findOne({ where: { id: subscriptionId } })
-        const deleteSubscription = await Subscription.update({ isDeleted: true, deletedBy: deletedBy }, { where: { id: subscriptionId } })
-        const subscriptionDeleted = await Subscription.findOne({ where: { id: subscriptionId } })
-        res.status(201).json(subscriptionDeleted)
+  try {
+    const isDeleted = await Subscription.findOne({
+      where: { id: subscriptionId },
+    });
+    const deleteSubscription = await Subscription.update(
+      { isDeleted: true, deletedBy: deletedBy },
+      { where: { id: subscriptionId } }
+    );
+    const subscriptionDeleted = await Subscription.findOne({
+      where: { id: subscriptionId },
+    });
+    res.status(201).json(subscriptionDeleted);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+
+exports.getAllSubscription = async (req, res) => {
+  const sequelize = require("sequelize");
+  try {
+    const users = await Subscription.findAll({});
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+exports.getSubscriptionById = async (req, res) => {
+  // res.send("all session");
+  const subsId = req.params.id;
+  try {
+    const subsById = await Subscription.findOne({
+      where: { id: subsId},
+    });
+console.log('subsById',subsById);
+    if (subsById) {
+      res.status(200).json(subsById);
     }
-    catch (e) {
-        res.status(400).json(e)
+    if (!subsById) {
+      res.status(404).json("subsId not Found!");
     }
-}
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
--- routes/subscription.router.js
+++ routes/subscription.router.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { webProtection } = require('../helper/auth')
-const { createSubcsription, updateSubscription, deleteSubscription } = require('../controllers/subscription.controller')
+const { createSubcsription, updateSubscription, deleteSubscription ,getAllSubscription,getSubscriptionById} = require('../controllers/subscription.controller')
 const router = express.Router()
 
 router.post('/createsubscription', webProtection, createSubcsription)
 router.put('/updatesubscription/:id', webProtection, updateSubscription)
 router.delete('/deletesubscription/:id', webProtection, deleteSubscription)
-
+router.get('/getsubscription', webProtection, getAllSubscription)
+router.get("/getsubscriptionbyid/:id", getSubscriptionById)
 
 
 module.exports = router
