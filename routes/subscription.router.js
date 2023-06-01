3884766d5c91088cbe8f23c0ad536f731d868751
varun <varun.mangoit@gmail.com>
2023-06-01 17:00:31 +0530

    work on subscription

--- routes/subscription.router.js
+++ routes/subscription.router.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const { webProtection } = require('../helper/auth')
-const { createSubcsription, updateSubscription, deleteSubscription ,getAllSubscription,getSubscriptionById,getSubscriptionByUserId,getSubscriptionSearchByUserId} = require('../controllers/subscription.controller')
-const router = express.Router()
+const express = require("express");
+const { webProtection } = require("../helper/auth");
+const {
+  createSubcsription,
+  updateSubscription,
+  deleteSubscription,
+  getAllSubscription,
+  getSubscriptionById,
+  getSubscriptionByUserId,
+  getSubscriptionSearchByUserId,
+  updateSubscriptionStatus,
+} = require("../controllers/subscription.controller");
+const router = express.Router();
 
-router.post('/createsubscription', webProtection, createSubcsription)
-router.put('/updatesubscription/:id', webProtection, updateSubscription)
-router.delete('/deletesubscription/:id', webProtection, deleteSubscription)
-router.get('/getsubscription', webProtection, getAllSubscription)
-router.get("/getsubscriptionbyid/:id", getSubscriptionById)
-router.get("/getsubscriptionbyuserid/:id", getSubscriptionByUserId)
-router.post("/getsubscriptionbyuserid/:search?", webProtection, getSubscriptionSearchByUserId)
+router.post("/createsubscription", webProtection, createSubcsription);
+router.put("/updatesubscription/:id", webProtection, updateSubscription);
+router.put(
+  "/updatesubscriptionstatus/:id",
+  webProtection,
+  updateSubscriptionStatus
+);
+router.delete("/deletesubscription/:id", webProtection, deleteSubscription);
+router.get("/getsubscription", webProtection, getAllSubscription);
+router.get("/getsubscriptionbyid/:id", getSubscriptionById);
+router.get("/getsubscriptionbyuserid/:id", getSubscriptionByUserId);
+router.post(
+  "/getsubscriptionbyuserid/:search?",
+  webProtection,
+  getSubscriptionSearchByUserId
+);
 
-
-
-module.exports = router
-
-
+module.exports = router;
